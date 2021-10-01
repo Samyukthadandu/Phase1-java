067bf68bb8c14739a627d1102ca758c289a70a28
Sam <samyukthadanduu@gmail.com>
2021-10-01 14:12:44 +0530

    First version of Login servlet proj

---
+++ LoginProject/loginlogic/phase2/LoginServlet.java
@@ -0,0 +1,63 @@
+package phase2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		RequestDispatcher rd = null;
+		if(username.equalsIgnoreCase("Sam")&& password.equalsIgnoreCase("abc123")) {
+			rd=request.getRequestDispatcher("SuccesspageServlet");
+			rd.forward(request, response);
+		}else {
+			rd=request.getRequestDispatcher("index.jsp");
+			PrintWriter out =response.getWriter();
+			rd.include(request, response);
+			out.println("<center> <span style ='color.red'>Invalid Credentials!!</span></center>");			
+				
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ LoginProject/src/main/webapp/index.jsp
@@ -0,0 +1,27 @@
+<html>
+<style>
+
+label{         
+                      
+           
+            width: 100px;
+            display: inline-block;
+            text-align: right;
+         
+        }
+    
+</style>
+<body>
+<h1 style="font-size:30px" align="center"> Beautiful U</h1>
+<body style="background-color:	MintCream;">
+<div align = "center">
+<h2  style="background-color:DodgerBlue;"> Login</h2>
+<form action="LoginServlet" method="post"><br><br>
+<label for="username"> UserName:</label> <input type="text" name="username" /><br/><br>
+<label for="password"> Password:</label>  <input type="password" name="password"/><br/><br>
+<input type="submit" value="Submit"><br/><br>
+
+</form>
+</div>	
+</body>
+</html>
